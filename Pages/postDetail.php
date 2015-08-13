0df4235a6340230a214f6c3d9a62b71a356a7be8
rarehare1220 <rarehare1220@gmail.com>
2015-08-13 19:37:29 +1000

    reply to reply

--- Pages/postDetail.php
+++ Pages/postDetail.php
@@ -1,5 +1,7 @@
 <?php
+
 use Parse\ParseQuery;
+
 //if (isset($_POST["logout"])) {
 //    ParseUser::logOut();
 //    $currentUser = ParseUser::getCurrentUser();
@@ -7,103 +9,118 @@ use Parse\ParseQuery;
 //}
 //
 ?>
-        <!-- CONTENT -->
-        <?php
-        //$postID = $_GET['pid'];
-        $postID = "WzPH1MSoGr";
-        $query = new ParseQuery("Post");
-        try {
-            $postResult = $query->get($postID);
-        } catch (ParseException $ex) {
-            // The object was not retrieved successfully.
-            // error is a ParseException with an error code and message.
-        }
+<!-- CONTENT -->
+<?php
+//$postID = $_GET['pid'];
+$postID = "WzPH1MSoGr";
+$query = new ParseQuery("Post");
+try {
+    $postResult = $query->get($postID);
+} catch (ParseException $ex) {
+    // The object was not retrieved successfully.
+    // error is a ParseException with an error code and message.
+}
 
-        $title = $postResult->get("title");
-        $content = $postResult->get("content");
-        $author = $postResult->get("author");
-        $author->fetch();
-        $unit = $postResult->get("unit");
-        $unit->fetch();
+$title = $postResult->get("title");
+$content = $postResult->get("content");
+$author = $postResult->get("author");
+$author->fetch();
+$unit = $postResult->get("unit");
+$unit->fetch();
 //            
-        $name = $author->get("name");
-        $code = $unit->get("code");
+$name = $author->get("name");
+$code = $unit->get("code");
 //array:
-        $imageArray = $postResult->get("mediaFiles");
+$imageArray = $postResult->get("mediaFiles");
 
-        $imageCount = count($imageArray);
-            
-            for($x=0; $x < $imageCount; $x++)
-            {
-                $imageTag = "<#I#M#A#G#E#>" . $x . "</#I#M#A#G#E#>";
-                $imageUrl = $imageArray[$x]->getURL();
-                $imageUrlTag = "<br/><img src='" . $imageUrl . "'/><br/>";
-                $content = str_replace($imageTag, $imageUrlTag, $content);
-                //echo $content;
-            }
+$imageCount = count($imageArray);
+
+for ($x = 0; $x < $imageCount; $x++) {
+    $imageTag = "<#I#M#A#G#E#>" . $x . "</#I#M#A#G#E#>";
+    $imageUrl = $imageArray[$x]->getURL();
+    $imageUrlTag = "<br/><img src='" . $imageUrl . "'/><br/>";
+    $content = str_replace($imageTag, $imageUrlTag, $content);
+    //echo $content;
+}
+?>
+
+<div class="postDetails">
+    <!--Title-->
+    <div class="postTitle">
+        <?php echo $title; ?>
+    </div>
+    <!--Post Information-->
+    <div class="postInfo">
+        <?php echo $name; ?>
+    </div>
+    <!--Post Content-->
+    <div class="postContent">
+        <p>
+            <?php
+            echo $content;
+            ?>
+        </p>
+    </div>
+</div>
+<!--Post Comment-->
+<div class="postComment">
+    <?php
+    $queryReply = new ParseQuery("Reply");
+    $queryReply->includeKey("author");
+    $queryReply->includeKey("replyTo.author");
+    $queryReply->equalTo("belongTo", $postResult);
+    $replyList = $queryReply->find();
+    //$replyList->fetchAll();
+
+    $replyCount = $queryReply->count();
+    ?>
+    <div class="comment-Header">
+        <?php
+        echo $replyCount . "个回复";
         ?>
-        
-        <div class="postDetails">
-            <!--Title-->
-            <div class="postTitle">
-                <?php echo $title; ?>
-            </div>
-            <!--Post Information-->
-            <div class="postInfo">
-                <?php echo $name; ?>
-            </div>
-            <!--Post Content-->
-            <div class="postContent">
-                <p>
-                    <?php
-                    echo $content;
-                    ?>
-                </p>
-            </div>
-        </div>
-        <!--Post Comment-->
-        <div class="postComment">
-            <?php
-                $queryReply = new ParseQuery("Reply");
-                $queryReply->includeKey("author");
-                $queryReply->equalTo("belongTo", $postResult);
-                $replyList = $queryReply -> find();
-                //$replyList->fetchAll();
-                
-                $replyCount = $queryReply -> count();
-            ?>
-            <div class="comment-Header">
-                <?php
-                    echo $replyCount . "个回复";
-                ?>
-            </div>
-            <?php
-                for($i = 0; $i < $replyCount; $i++){
-                    $replyAuthor = $replyList[$i]->get("author");
-                    //$replyAuthor->fetch();
-                    $name = $replyAuthor->get("name");
-                    //var_dump($replyList[$i]->getCreatedAt());
-                    //$date = date("Y-m-d H:i", $replyList[$i]->getCreatedAt());
-                    $date = date_format($replyList[$i]->getCreatedAt(), "Y-m-d H:i:s");
-                    $replyContent = $replyList[$i]->get("replyContent");
-                    
-                    echo "<div class=\"replyStyle\">";
-                    echo "<div class=\"replyAuthor\">";
-                    echo "<span class=\"glyphicon glyphicon-user\"></span>";
-                    echo " ";
-                    echo $name;
-                    echo "&nbsp;&nbsp;&nbsp;&nbsp;";
-                    echo "<span class=\"glyphicon glyphicon-time\"></span>";
-                    echo " ";
-                    echo $date;
-                    echo "</div>";
-                    
-                    echo "<div class=\"replyContent\">";
-                    echo $replyContent;
-                    echo "</div>";
-                    echo "<br>";
-                    echo "</div>";
-                }
-                
-            ?>
-        </div>
+    </div>
+    <?php
+    for ($i = 0; $i < $replyCount; $i++) {
+        $replyTo = $replyList[$i]->get("replyTo");
+        if (empty($replyTo)) {
+            // echo "Reply To is: " . $replyToAuthorName;
+            $replyAuthor = $replyList[$i]->get("author");
+            //$replyAuthor->fetch();
+            $name = $replyAuthor->get("name");
+            //var_dump($replyList[$i]->getCreatedAt());
+            //$date = date("Y-m-d H:i", $replyList[$i]->getCreatedAt());
+            $date = date_format($replyList[$i]->getCreatedAt(), "Y-m-d H:i:s");
+            $replyContent = $replyList[$i]->get("replyContent");
+
+            echo "<div class=\"replyStyle\">";
+            echo "<div class=\"replyAuthor\">";
+            echo "<span class=\"glyphicon glyphicon-user\"></span>";
+            echo " ";
+            echo $name;
+            echo "&nbsp;&nbsp;&nbsp;&nbsp;";
+            echo "<span class=\"glyphicon glyphicon-time\"></span>";
+            echo " ";
+            echo $date;
+            echo "</div>";
+
+            echo "<div class=\"replyContent\">";
+            echo $replyContent;
+            echo "</div>";
+            echo "<br>";
+            echo "</div>";
+            //------------------------------------------------------------------------------------
+            //回复的回复：
+//            echo "<div class=\"replyToReply\">";
+//            echo "--------回复的回复--------";
+//
+//
+//
+//
+//
+//            echo "</div>";
+        } else {
+            echo "空空空空";
+        }
+    }
+    ?>
+</div>
