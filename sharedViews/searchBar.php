0b2fb548caa4d92abacc52410965ef1e78d578b7
Zhuoyi Guo <joeysidea@hotmail.com>
2015-08-14 23:38:10 +1000

    nothing

--- sharedViews/searchBar.php
+++ sharedViews/searchBar.php
@@ -3,7 +3,6 @@
     <input type="text" id="query" name="q"/>
     <input type="submit" value="Search"/>
     </div>
-    <div id="livesearch"></div>
 </form>
 <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
 <script src="//code.jquery.com/jquery-1.10.2.js"></script>
